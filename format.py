fb7c840e306b961bea179008a8d343295510f326
GeorgeYZJ <46706151+GeorgeYZJ@users.noreply.github.com>
2020-08-28 16:33:37 +0800

    Update format.py

--- format.py
+++ format.py
@@ -4,18 +4,21 @@ from datetime import datetime
 
 
 def formatTime(fname):
-    time = []
+    time = {}
     path = './2020-08-26 10_08_25.249321/' + str(fname)
     with open(path , 'r') as csvfile:
         data = list(csv.reader(csvfile))
         data = data[1:]
         for i in data:
             try:
-                time.append(datetime.strptime(i[1], '%b-%d-%y %H:%M%p'))
+                key = datetime.strptime(i[1], '%b-%d-%y %H:%M%p')
+                time[str(key)] = i[2]
             except:
-                time.append(datetime.strptime(i[1], '%H:%M%p').time())
+                key = datetime.strptime(i[1], '%H:%M%p').time()
+                time[str(key)] = i[2]
     return (time)
 
 # if __name__ == '__main__':
 #     a = formatTime('ABT.csv')
-#     print(a)
+#     for i,j in a.items():
+#         print(i + '  ' + j)
