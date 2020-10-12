b376abcbb9057c176d13f553c92410988cab4d9f
Luke C <43847967+kingcretin@users.noreply.github.com>
2020-10-12 15:46:01 +0800

    Updated Tracker class
    Updated the track_movements.py final to switch from numerical outbut to discrete.

--- track_movements.py
+++ track_movements.py
@@ -10,13 +10,19 @@ class Tracker:
         self.data = reader.get_data_yahoo(ticker, timestamp, timestamp + timedelta(days=3))
 
     def get_price_change(self):
-        return round(((self.data['Adj Close'][-1]-self.data['Adj Close'][0])/self.data['Adj Close'][0]*100),2)
-    
+        change = round(((self.data['Adj Close'][-1]-self.data['Adj Close'][0])/self.data['Adj Close'][0]*100),2)
+        print(change)
+        return_value = 0
+        if change > 2:
+            return_value = 1
+        elif change < -2:
+            return_value = -1
+        return return_value
 
 ## Example Usage
 # Define your datetime object
 #datetime_object = datetime.datetime.strptime('2020-06-17 06:18:00', '%Y-%m-%d %H:%M:%S')
 # Get the object
-#new_tracker = Tracker(datetime_object, 'ABT')
+#new_tracker = Tracker(datetime_object, 'DIS')
 # Call the get_price_change function on it
 #print(new_tracker.get_price_change())
